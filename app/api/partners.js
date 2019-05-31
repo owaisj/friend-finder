9136b4a179328461d99c20c09ed3c5f7b8005bd5
owaisj <jamil.owais@gmail.com>
2019-05-31 14:22:17 -0500

    Array of objects with Pokemon and MBTI type

---
+++ app/api/partners.js
@@ -0,0 +1,67 @@
+//TODO: Survey Metrics Beyond MBTI
+module.exports = [
+    {
+        name: "Absol",
+        type: "INTJ"
+    },
+    {
+        name: "Alakazam",
+        type: "INTP"
+    },
+    {
+        name: "Espeon",
+        type: "INFJ"
+    },
+    {
+        name: "Bulbasaur",
+        type: "INFP"
+    },
+    {   
+        name: "Lucario",
+        type: "ISTJ"
+    },
+    {
+        name: "Piplup",
+        type: "ISFJ"
+    },
+    {
+        name: "Greninja",
+        type: "ISTP"
+    },
+    {
+        name: "Marowak",
+        type: "ISFP"
+    },
+    {   
+        name: "Squirtle",
+        type: "ENTJ"
+    },
+    {
+        name: "Rotom",
+        type: "ENTP"
+    },
+    {
+        name: "Meowth",
+        type: "ENFJ"
+    },
+    {
+        name: "Sylveon",
+        type: "ENFP"
+    },
+    {
+        name: "Roserade",
+        type: "ESTJ"
+    },
+    {
+        name: "Charmander",
+        type: "ESFJ"
+    },
+    {
+        name: "Floatzel",
+        type: "ESTP"
+    },
+    {
+        name: "Arcanine",
+        type: "ESFP"
+    }
+]
