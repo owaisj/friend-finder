70fdee56c59c5c74728d10cd44896c9ef9f6ac96
owaisj <jamil.owais@gmail.com>
2019-05-31 14:32:29 -0500

    GET route using params

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -10,6 +10,19 @@ router.get('/partners', function(request, response){
     return response.json(partners);
 })
 
+//GET/Read
+router.get('/partners/:type', (request, response) => {
+    console.log(request.params.type);
+    let match = '';
+    for (let pokemon in partners) {
+         if (partners[pokemon]['type'] === request.params.type) {
+             match = partners[pokemon];
+             break;
+         }
+    }
+    response.json(match);
+})
+
 //TODO: POST route to handle incoming survey results (form)
 router.post('/partners', function(request, response){
    console.log('User sent the MBTI of', request.body.type);
