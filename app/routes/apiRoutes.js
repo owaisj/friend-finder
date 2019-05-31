a070e3d5f9ecd9d75e59a8efdd11740d728dae24
owaisj <jamil.owais@gmail.com>
2019-05-31 14:28:46 -0500

    POST route with for...in loop and json response

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -10,7 +10,17 @@ router.get('/partners', function(request, response){
     return response.json(partners);
 })
 
-
-//TODO: POST route
+//TODO: POST route to handle incoming survey results (form)
+router.post('/partners', function(request, response){
+   console.log('User sent the MBTI of', request.body.type);
+   let match = '';
+   for (let pokemon in partners) {
+        if (partners[pokemon]['type'] === request.body.type) {
+            match = partners[pokemon];
+            break;
+        }
+   }
+   response.json(match);
+})
 
 module.exports = router;
